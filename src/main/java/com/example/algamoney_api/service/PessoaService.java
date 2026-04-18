e8b3be97109092636469a0ed536796c76b3463a0
caio castro <caio-rb@hotmail.com>
2026-04-18 19:09:34 -0300

    Atualização de campo específico

--- src/main/java/com/example/algamoney_api/service/PessoaService.java
+++ src/main/java/com/example/algamoney_api/service/PessoaService.java
@@ -23,4 +23,17 @@ public class PessoaService {
 	    return pessoaRepository.save(pessoaSalva);
 	}
 	
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		pessoaSalva.setAtivo(ativo);
+		pessoaRepository.save(pessoaSalva);
+	}
+	
+	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
+		  Pessoa pessoaSalva = pessoaRepository.findById(codigo)
+			    .orElseThrow(() -> new EmptyResultDataAccessException(1));
+		
+		  return pessoaSalva;
+	}
+	
 }
